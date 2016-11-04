03ca1013b9faafc0a3cc7f2499a1a777c3e420f0
kaeru <kaeru@kaeru-PC>
2016-11-04 12:50:45 +0900

    PC First Commit

--- RealtimeDataAggregation/apps/count/set_classpath.sh
+++ RealtimeDataAggregation/apps/count/set_classpath.sh
@@ -1,22 +1,22 @@
 #Setting CLASS PATH
  #Program setting file
-export CLASSPATH=$CLASSPATH:../property:../resource
+#export CLASSPATH=$CLASSPATH:../property:../resource
 
  #Apach
 export CLASSPATH=$CLASSPATH:\
-../library/pool/commons-pool2-2.4.2.jar:\
-../library/math/commons-math3-3.5.jar
+../../lib/pool/commons-pool2-2.4.2.jar:\
+../../lib/math/commons-math3-3.5.jar
 
  #Log setting file
 export CLASSPATH=$CLASSPATH:\
-../library/Log/logback-access-1.1.3.jar:\
-../library/Log/logback-classic-1.1.3.jar:\
-../library/Log/logback-core-1.1.3.jar:\
-../library/Log/slf4j-api-1.7.12.jar
+../../lib/Log/logback-access-1.1.3.jar:\
+../../lib/Log/logback-classic-1.1.3.jar:\
+../../lib/Log/logback-core-1.1.3.jar:\
+../../lib/Log/slf4j-api-1.7.12.jar
 
  #CSV setting file
 export CLASSPATH=$CLASSPATH:\
-../library/csv/opencsv-3.5.jar
+../../lib/csv/opencsv-3.5.jar
 
 ##
 export CLASSPATH=`echo $CLASSPATH | tr ':' '\n' | sort -u | paste -d: -s -`;
