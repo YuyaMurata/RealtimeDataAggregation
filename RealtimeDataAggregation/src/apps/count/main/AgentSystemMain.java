cd0f188902a1beadd40fa87e9b559caf3569ee24
kaeru <kaeru@kaeru-PC>
2016-11-06 18:45:50 +0900

    PC First Commit

--- RealtimeDataAggregation/src/apps/count/main/AgentSystemMain.java
+++ RealtimeDataAggregation/src/apps/count/main/AgentSystemMain.java
@@ -38,8 +38,8 @@ public class AgentSystemMain {
         for(int i=0; i < 10; i++){
             List msgdata = new ArrayList();
             List data = new ArrayList();
-            data.add(0L);
-            for(int j=0; j < i; j++) data.add(1L);
+            data.add(0);
+            for(int j=0; j < i; j++) data.add(1);
             
             msgdata.add(data);
             
