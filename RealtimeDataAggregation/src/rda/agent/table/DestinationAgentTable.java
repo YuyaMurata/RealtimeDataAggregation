50b93008445f3cb823c0bd80d7b79207542ebcc8
kaeru <kaeru@kaeru-PC>
2016-11-26 23:27:14 +0900

    PC First Commit

--- RealtimeDataAggregation/src/rda/agent/table/DestinationAgentTable.java
+++ RealtimeDataAggregation/src/rda/agent/table/DestinationAgentTable.java
@@ -45,7 +45,7 @@ public class DestinationAgentTable {
     }
     
     public Object getDestAgentID(Object id){
-        Integer hashID = id.toString().hashCode();
+        Integer hashID = id.hashCode();
         List destAgList = destTable.get(idList.get(hashID % size));
         return destAgList.get(hashID % destAgList.size());
     }
