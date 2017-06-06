7e66338ced447e3467a94da28263ebcb003cd143
kaeru <kaeru@LAPTOP-JLUFA49E>
2017-06-06 16:51:20 +0900

    note PC

--- RealtimeDataAggregation/src/apps/count/agent/aggregate/updator/UpdateAggregateAgent.java
+++ RealtimeDataAggregation/src/apps/count/agent/aggregate/updator/UpdateAggregateAgent.java
@@ -15,47 +15,49 @@ import rda.agent.updator.AgentUpdator;
  *
  * @author kaeru
  */
-public class UpdateAggregateAgent extends AgentUpdator{ 
-    private static final String AGENT_TYPE = "aggregateagent";
-    private static final String MESSAGE_TYPE = "updateAggregateAgent";
+public class UpdateAggregateAgent extends AgentUpdator {
 
-    public UpdateAggregateAgent() {
-    }
-    
-    public UpdateAggregateAgent(AgentKey agentKey, List data) {
-        super(agentKey, MESSAGE_TYPE, data);
-    }
-    
-    @Override
-    public void update(AgentClient client, Object agID, List data) {
-        if(data == null) return;
-        
-	System.out.println(agID+":"+client);
+	private static final String AGENT_TYPE = "aggregateagent";
+	private static final String MESSAGE_TYPE = "updateAggregateAgent";
+
+	public UpdateAggregateAgent() {
+	}
+
+	public UpdateAggregateAgent(AgentKey agentKey, List data) {
+		super(agentKey, MESSAGE_TYPE, data);
+	}
+
+	@Override
+	public void update(AgentClient client, Object agID, List data) {
+		if (data == null) {
+			return;
+		}
+
+		try {
+			AgentKey agentKey = new AgentKey(AGENT_TYPE, new Object[]{agID});
+
+			UpdateAggregateAgent executor = new UpdateAggregateAgent(agentKey, data);
+
+			Object reply = client.execute(agentKey, executor);
+
+			//System.out.println("Update "+agID+" = "+reply);
+		} catch (AgentException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void update(Object agID, List data) {
+		AgentKey agentKey = new AgentKey(AGENT_TYPE, new Object[]{agID});
 		
-        try {    
-            AgentKey agentKey = new AgentKey(AGENT_TYPE, new Object[]{agID});
-                
-            UpdateAggregateAgent executor = new UpdateAggregateAgent(agentKey, data);
-            
-            Object reply = client.execute(agentKey, executor);
-            
-            //System.out.println("Update "+agID+" = "+reply);
-            
-        } catch (AgentException e) {
-            e.printStackTrace();
-        } 
-    }
+		System.out.println(agID+" Update!");
 
-    @Override
-    public void update(Object agID, List data) {
-        AgentKey agentKey = new AgentKey(AGENT_TYPE, new Object[]{agID});
+		//Update Agent
+		UpdateAggregateAgent executor = new UpdateAggregateAgent(agentKey, data);
 
-        //Update Agent
-        UpdateAggregateAgent executor = new UpdateAggregateAgent(agentKey, data);
+		Object reply = executor.execute();
 
-        Object reply = executor.execute();
-        
-        String msg = "Update [" + agentKey + "] was updated. Reply is [" + reply + "]";
-    }
-    
+		String msg = "Update [" + agentKey + "] was updated. Reply is [" + reply + "]";
+	}
+
 }
