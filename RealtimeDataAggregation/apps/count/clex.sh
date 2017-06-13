d6bd4e89f791033e33b6cc1e5882f9099b70078b
kaeru <kaeru@LAPTOP-JLUFA49E>
2017-06-13 17:46:29 +0900

    note PC

--- RealtimeDataAggregation/apps/count/clex.sh
+++ RealtimeDataAggregation/apps/count/clex.sh
@@ -1,1 +1,5 @@
-clsql -user:cetaadmin -password:cetaadmin
+if [ $# -ne 1 ]; then
+    clsql -catalog:$1:11000 -user:cetaadmin -password:cetaadmin
+else
+  clsql -user:cetaadmin -password:cetaadmin  
+fi
