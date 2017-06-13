ecebe3a00b3cc970b622fb453366f9c637d75948
kaeru <kaeru@LAPTOP-JLUFA49E>
2017-06-13 17:50:39 +0900

    note PC

--- RealtimeDataAggregation/apps/count/clex.sh
+++ RealtimeDataAggregation/apps/count/clex.sh
@@ -1,5 +1,5 @@
 if [ $# -ne 1 ]; then
-    clsql -catalog:$1:11000 -user:cetaadmin -password:cetaadmin
+    echo clsql -catalog:$1:11000 -user:cetaadmin -password:cetaadmin
 else
-  clsql -user:cetaadmin -password:cetaadmin  
+    echo clsql -user:cetaadmin -password:cetaadmin  
 fi
