ef806e0ebbb73434519eaee20cda0286d2ebdfc5
kaeru <kaeru@LAPTOP-JLUFA49E>
2017-06-14 15:56:44 +0900

    note PC

--- RealtimeDataAggregation/src/apps/count/main/AgentSystemMain.java
+++ RealtimeDataAggregation/src/apps/count/main/AgentSystemMain.java
@@ -162,7 +162,6 @@ public class AgentSystemMain {
 
 				//System.out.println(agID+" - "+user.toString());
 				((WindowStream)serverMap.get(scManager.getDistributedServer(agID))).in(agID, user);
-				System.out.println("agID="+agID+" server="+scManager.getDistributedServer(agID)+" ws="+((WindowStream)serverMap.get(scManager.getDistributedServer(agID))));
 				totalData++;
 			}
 		} catch (TimeOverEvent ex) {
