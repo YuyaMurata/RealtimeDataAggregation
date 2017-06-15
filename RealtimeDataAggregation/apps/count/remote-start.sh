fb1107fd4ceef729ac9702879e7b41e4f8a0426a
kaeru <kaeru@LAPTOP-JLUFA49E>
2017-06-15 15:04:50 +0900

    note PC

--- RealtimeDataAggregation/apps/count/remote-start.sh
+++ RealtimeDataAggregation/apps/count/remote-start.sh
@@ -1,10 +1,5 @@
 for i in 1 2 3 4 5 6
 do
-    #確認
-    sshpass -p 11m35584 ssh root@h$i 'echo `hostname`'
-    echo "確認後 Enter"
-    read
-
     #遠隔で実験用スクリプトを起動
     sshpass -p 11m35584 ssh root@$1 \
     "source /etc/profile;\
