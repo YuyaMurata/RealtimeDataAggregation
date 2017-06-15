a37dcef02a4fc56637afaeecc3ac2bf61a533837
kaeru <kaeru@LAPTOP-JLUFA49E>
2017-06-15 14:54:41 +0900

    note PC

--- RealtimeDataAggregation/apps/count/remote-start.sh
+++ RealtimeDataAggregation/apps/count/remote-start.sh
@@ -1,16 +1,15 @@
-#確認
-sshpass -p 11m35584 ssh root@$1 'echo `hostname`'
-echo "確認後 Enter"
-read
+for i in 1 2 3 4 5 6
+do
+    #確認
+    sshpass -p 11m35584 ssh root@$i 'echo `hostname`'
+    echo "確認後 Enter"
+    read
 
-#遠隔で実験用スクリプトを起動
-sshpass -p 11m35584 ssh root@$1 \
-"source /etc/profile;\
-cd $CETA_HOME/App/AgentSystem/AgentSystem/setting;\
-./git-remote.sh;\
-chmod 777 *.sh;\
-./remote-script.sh"
-
-#ログをDropboxに送る
-dropbox upload $CETA_HOME/App/dropbox_log/*.zip logs
-mv $CETA_HOME/App/dropbox_log/*.zip $CETA_HOME/App/dropbox_log/old/
+    #遠隔で実験用スクリプトを起動
+    sshpass -p 11m35584 ssh root@$1 \
+    "source /etc/profile;\
+    cd $CETA_HOME/App/AgentSystem/RealtimeDataAggregation/RealtimeDataAggregation/app/count;\
+    chmod 777 *.sh;\
+    killall -9 java;\
+    ./start.sh"
+done
