e198a21c67a4725e853cd1db4a60e2b989163557
kaeru <kaeru@LAPTOP-JLUFA49E>
2017-06-16 13:28:59 +0900

    note PC

--- RealtimeDataAggregation/apps/count/remote-start.sh
+++ RealtimeDataAggregation/apps/count/remote-start.sh
@@ -6,7 +6,7 @@ do
     cd $CETA_HOME/App/AgentSystem/RealtimeDataAggregation/RealtimeDataAggregation/apps/count;\
     chmod 777 *.sh;\
     killall -9 java;\
-   ./start.sh  `hostname`" &
+   ./start.sh 'hostname'" 
 done
 
 #確認
