e4765925aa17fc31053aef63ef3552813d164fdd
kaeru <kaeru@LAPTOP-JLUFA49E>
2017-06-16 11:38:47 +0900

    note PC

--- RealtimeDataAggregation/apps/count/remote-start.sh
+++ RealtimeDataAggregation/apps/count/remote-start.sh
@@ -13,8 +13,8 @@ done
 #for i in 1
 #do
     #遠隔で実験用スクリプトを起動
-#    sshpass -p 11m35584 ssh root@h$i \
-#    "source /etc/profile;\
-#    cd $CETA_HOME/App/AgentSystem/RealtimeDataAggregation/RealtimeDataAggregation/apps/count;\
-#    showRegion"
+    sshpass -p 11m35584 ssh root@h$i \
+    "source /etc/profile;\
+    cd $CETA_HOME/App/AgentSystem/RealtimeDataAggregation/RealtimeDataAggregation/apps/count;\
+    showRegion"
 #done
